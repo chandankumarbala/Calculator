c657fe7e841af384712777963ba630d36bab410d
chandanbala <chandan77.777@gmail.com>
2016-11-22 10:28:41 +0530

    today

--- src/com/dbs/code/Calculator.java
+++ src/com/dbs/code/Calculator.java
@@ -5,8 +5,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
+	
+	private Stack operationStack;
+	public Calculator(){
+		operationStack= new Stack();
+	}
+	
+	private float popStack(){
+		return Float.valueOf(operationStack.pop().toString());
+	}
+	private void pushStack(Object data){
+		operationStack.push(data);
+	}
+	private float publishResult() throws Exception{
+		if(operationStack.size()==1)
+			return popStack();
+		else
+			throw new Exception("Please insert a valid input");
+	}
 
-	private static boolean detectOperation(String data){
+	private static boolean isOperator(String data){
 		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
 		Matcher m = p.matcher(data);
 		boolean result = m.find();
@@ -16,70 +34,58 @@ public class Calculator {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String data="30,40,+";//args[0];
+		
 	}
 	
 	
 
 	public float calculate(String expression) throws Exception{
 		// TODO Auto-generated method stub
-		
-		String trimmedString=expression.trim();
-		if(trimmedString.length()<3){
-			throw new Exception("Please insert the data in form of operand,operand,operator");
-		}
 		String[] argsArr=expression.split(",");
-		if(detectOperation(argsArr[argsArr.length-1]) && detectOperation(argsArr[argsArr.length-2])){
-			throw new Exception("Please insert a valid input");
-		}
-		
-		Float result;	
-		
-		Stack operationStack = new Stack();
 		for(int i=0;i<argsArr.length;i++){
-			float temp;
-			if(!detectOperation(argsArr[i])){
-				operationStack.push(argsArr[i]);
-				
-			}else if(argsArr[i].equals("!")){
-				temp=calculateFactorial(operationStack.pop().toString());
-				
-				
-			}
-			else{
-				float stOperand=Float.valueOf(operationStack.pop().toString());
-				float ndoperand=Float.valueOf(operationStack.pop().toString());
-				if(argsArr[i].toString().equals("+")){
-					temp=stOperand+ndoperand;
-					operationStack.push(temp);
-				}else if(argsArr[i].toString().equals("-")){
-					temp=ndoperand-stOperand;
-					operationStack.push(temp);
-				}else if(argsArr[i].toString().equals("/")){
-					temp=ndoperand/stOperand;
-					operationStack.push(temp);
-				}else if(argsArr[i].toString().equals("*")){
-					temp=ndoperand*stOperand;
-					operationStack.push(temp);
-				}else if(argsArr[i].toString().equals("%")){
-					temp=ndoperand%stOperand;
-					operationStack.push(temp);
-				}
-				
-				
-			}
+			
+			if(!isOperator(argsArr[i]))
+				pushStack(argsArr[i]);
+			else
+				performBinaryOperation(argsArr[i]);
 		}
 		
-		if(operationStack.size()!=1)
-			throw new Exception("Please insert a valid input");
-		
-		result=(Float)operationStack.pop();
-		return result;
+		return publishResult();
 		
 	}
+	
+	
+	private void performBinaryOperation(Object operator) throws Exception{
 
-
+		if(operationStack.size()<2)
+			throw new Exception("Please insert a valid input");
+		
+		float operationResult=0;
+		float firstOperand=popStack();
+		float secondOperand=popStack();
+		
+		if(operator.toString().equals("+")){
+			operationResult=firstOperand+secondOperand;
+			
+		}else if(operator.toString().equals("-")){
+			operationResult=secondOperand-firstOperand;
+			
+		}else if(operator.toString().equals("/")){
+			operationResult=secondOperand/firstOperand;
+			
+		}else if(operator.toString().equals("*")){
+			operationResult=secondOperand*firstOperand;
+			
+		}else if(operator.toString().equals("%")){
+			operationResult=secondOperand%firstOperand;
+			
+		}
+		pushStack(operationResult);
+		
+	
+	}
+	
+	
 	private float calculateFactorial(String fac) throws Exception{
 		// TODO Auto-generated method stub
 		int fact = 0;
--- src/com/dbs/test/TestCalculator.java
+++ src/com/dbs/test/TestCalculator.java
@@ -4,48 +4,44 @@ import org.junit.Test;
 
 import com.dbs.code.Calculator;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import junit.framework.TestCase;
 
 public class TestCalculator extends TestCase {
 	
-
+	Calculator cal=new Calculator();
+	
 	public void testAddition() throws Exception{
 		String expression="15,30,+";
-		Calculator cal=new Calculator();
 		assertEquals(45.0f,cal.calculate(expression));
 		
 	}
 	public void testAdditionI() throws Exception{
 		String expression="30,15,+";
-		Calculator cal=new Calculator();
+		
 		assertEquals(45.0f,cal.calculate(expression));
 		
 	}
 	public void testDivision() throws Exception{
 		String expression="30,15,/";
-		Calculator cal=new Calculator();
+		 
 		assertEquals(2.0f,cal.calculate(expression));
 		
 	}
 	public void testDivisionI() throws Exception{
 		String expression="15,30,/";
-		Calculator cal=new Calculator();
+		 
 		assertEquals(0.5f,cal.calculate(expression));
 		
 	}
 	public void testMul() throws Exception{
 		String expression="15,30,*";
-		Calculator cal=new Calculator();
+		 
 		assertEquals(450.0f,cal.calculate(expression));
 		
 	}
 	public void testPercentile() throws Exception{
 		String expression="15,30,%";
-		Calculator cal=new Calculator();
+		 
 		assertEquals(15.0f,cal.calculate(expression));
 		
 	}
@@ -53,24 +49,15 @@ public class TestCalculator extends TestCase {
 	
 	public void testAdditionIC() throws Exception{
 		String expression="30,20,+";
-		Calculator cal=new Calculator();
+	 
 		assertEquals(50.0f,cal.calculate(expression));
 		
 	}
 	
-	
-	public void testAdditionIE() throws Exception{
-		String expression="";
-		Calculator cal=new Calculator();
-		try{
-			cal.calculate(expression);
-		}catch(Exception e){
-		assertEquals("Please insert the data in form of operand,operand,operator",e.getMessage());
-		}
-	}
+
 	public void testAdditionIE1() throws Exception{
 		String expression="30,20,+,1,-";
-		Calculator cal=new Calculator();
+		 
 		try{
 			cal.calculate(expression);
 		}catch(Exception e){
@@ -80,7 +67,7 @@ public class TestCalculator extends TestCase {
 	
 	public void testAdditionIE2() throws Exception{
 		String expression="30,20,+,1,-,/";
-		Calculator cal=new Calculator();
+		 
 		try{
 			cal.calculate(expression);
 		}catch(Exception e){
